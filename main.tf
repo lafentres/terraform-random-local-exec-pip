7bbb38de1e3f6ec46f3f592df6075aa2cd2cc88a
Krista LaFentres (she/her) <lafentres@users.noreply.github.com>
2025-02-18 14:17:51 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "random_id" "random" {
+  keepers = {
+    uuid = uuid()
+  }
+
+  byte_length = 12
+
+  provisioner "local-exec" {
+      command = "aws --version"
+  }
+}
+
+output "random" {
+  value = random_id.random.hex
+}
